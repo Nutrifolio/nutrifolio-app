396078356bff65ce6a108672833192843557c1e5
sotirangelo <sotirangelo@github.com>
2022-05-26 01:01:21 +0300

    Add all screens to tab navigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import HomeScreen from '../screens/HomeScreen';
+import SearchScreen from '../screens/SearchScreen';
+import ProfileScreen from '../screens/ProfileScreen'
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
     return (
         <Tab.Navigator>
-
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+            />
+            <Tab.Screen
+                name="Search"
+                component={SearchScreen}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+            />
         </Tab.Navigator>
     );
 };
