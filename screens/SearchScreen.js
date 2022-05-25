06a05727f8b4645eac26904588205e1593eef137
sotirangelo <sotirangelo@github.com>
2022-05-26 00:55:55 +0300

    Create empty SearchScreen

---
+++ screens/SearchScreen.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import Screen from '../components/Screen';
+
+const SearchScreen = () => {
+    return (
+        <Screen>
+            <View style={styles.container}>
+                <Text>SearchScreen</Text>
+            </View>
+        </Screen>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
+
+export default SearchScreen;
