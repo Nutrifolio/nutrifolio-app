c49f01e5a1def26f177397c8c318d617a9513866
sotirangelo <t8190001@aueb.gr>
2022-06-11 21:18:43 +0300

    Add navigation in HomeScreen for testing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 
 import Screen from '../components/Screen';
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
     return (
         <Screen>
             <View style={styles.container}>
                 <Text>HomeScreen</Text>
+                <Button
+                    title="Go to product"
+                    onPress={() => props.navigation.navigate('Product')}
+                />
+                <Button
+                    title="Go to store"
+                    onPress={() => props.navigation.navigate('Store')}
+                />
             </View>
         </Screen>
     );
