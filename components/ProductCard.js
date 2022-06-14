ae88403292a1ba53f42cca1ecdfaa19e209b1907
sotirangelo <t8190001@aueb.gr>
2022-06-15 02:22:13 +0300

    Add text and styling to ProductCard

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,12 +1,15 @@
 import { View, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import propTypes from 'prop-types'
+import NutriText from './NutriText'
+import colors from '../styles/colors'
 
 const ProductCard = (props) => {
   return (
     <View style={styles.container}>
       <View style={styles.detailsContainer}>
-          
+          <NutriText style={styles.title}>{props.title}</NutriText>
+          <NutriText style={styles.description}>{props.description}</NutriText>
       </View>
       <View style={styles.imageContainer}>
         <Image source={{uri: props.image}} style={styles.image} />
@@ -17,19 +20,29 @@ const ProductCard = (props) => {
 
 const styles = StyleSheet.create({
     container: {
-      height: 180,
+      height: 160,
       width: '100%',
       flexDirection: 'row',
       justifyContent: 'space-between',
       padding: 20,
-      backgroundColor: 'yellow'
+      backgroundColor: 'blue'
     },
     detailsContainer: {
       height: '100%',
       width: '65%',
-      backgroundColor: 'blue',
+      backgroundColor: 'yellow',
       flexDirection: 'column',
       
+    },
+    title: {
+      fontWeight: '600',
+      fontSize: 16,
+      paddingBottom: 3
+    },
+    description: {
+      fontSize: 12,
+      color: colors.grey
+
     },
     imageContainer: {
       height: '100%',
@@ -39,7 +52,7 @@ const styles = StyleSheet.create({
       alignItems: 'center'
     },
     image: {
-      height: '60%',
+      height: '70%',
       width: '85%',
       resizeMode: 'cover',
       borderRadius: 10
