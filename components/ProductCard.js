74cb0e3dd9e54f2b60012dce588c07869e74dd69
sotirangelo <t8190001@aueb.gr>
2022-06-18 20:56:55 +0300

    Add numeric details in ProductCard

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -3,13 +3,29 @@ import React from 'react'
 import propTypes from 'prop-types'
 import NutriText from './NutriText'
 import colors from '../styles/colors'
+import { FontAwesome } from '@expo/vector-icons'
 
 const ProductCard = (props) => {
   return (
     <View style={styles.container}>
       <View style={styles.detailsContainer}>
+
           <NutriText style={styles.title}>{props.title}</NutriText>
           <NutriText numberOfLines={2} style={styles.description}>{props.description}</NutriText>
+
+          <View style={styles.valuesContainer}>
+            <View style={styles.values}>
+              <NutriText>{props.calories}</NutriText>
+              <NutriText>kcal</NutriText>
+            </View>
+            <View style={styles.values}>
+              <NutriText>{props.price}</NutriText>
+              <FontAwesome name="euro" size={14} color="black" />
+            </View>
+            <View style={styles.values}>
+              <FontAwesome name="map-marker" size={14} color="black" />
+              <NutriText>{props.distance}</NutriText>
+            </View>
       </View>
       <View style={styles.imageContainer}>
         <Image source={{uri: props.image}} style={styles.image} />
@@ -30,8 +46,8 @@ const styles = StyleSheet.create({
     detailsContainer: {
       height: '100%',
       width: '65%',
-      backgroundColor: 'yellow',
       flexDirection: 'column',
+      justifyContent: 'space-evenly'
       
     },
     title: {
@@ -44,7 +60,15 @@ const styles = StyleSheet.create({
       color: colors.grey
 
     },
-    imageContainer: {
+    valuesContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    values: {
+      flexDirection: 'row',
+      fontSize: 14,
+      alignItems: 'center',
+    },
       height: '100%',
       width: '35%',
       backgroundColor: 'red',
