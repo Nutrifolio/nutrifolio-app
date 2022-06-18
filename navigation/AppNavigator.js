27712e9f481b0f10fc24a93af0110b3060499cb0
sotirangelo <t8190001@aueb.gr>
2022-06-18 21:14:50 +0300

    Update tab navigator style

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,27 +2,63 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import ProfileScreen from '../screens/ProfileScreen'
 import { HomeNavigator, SearchNavigator } from './ScreenNavigators';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useFonts, Rubik_400Regular } from "@expo-google-fonts/rubik";
+import colors from '../styles/colors';
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
+    let [fontsLoaded] = useFonts({ Rubik_400Regular });
+
     return (
         <Tab.Navigator
             screenOptions={{
-                headerShown: false
+                headerShown: false,
+                tabBarLabelStyle: fontsLoaded
+                ? {fontFamily: 'Rubik_400Regular'}
+                : {},
+                tabBarActiveTintColor: colors.primary
             }}
         >
             <Tab.Screen
                 name="Home"
                 component={HomeNavigator}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons 
+                            name="home" 
+                            size={size} 
+                            color={color}
+                        />
+                    )
+                }}
             />
             <Tab.Screen
                 name="Search"
                 component={SearchNavigator}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons 
+                            name="magnify" 
+                            size={size} 
+                            color={color}
+                        />
+                    )
+                }}
             />
             <Tab.Screen
                 name="Profile"
                 component={ProfileScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons 
+                            name="account" 
+                            size={size} 
+                            color={color}
+                        />
+                    )
+                }}
             />
         </Tab.Navigator>
     );
