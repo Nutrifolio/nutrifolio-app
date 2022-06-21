027be9436b95e16412d88d98a2ef230b95f71dd9
sotirangelo <t8190001@aueb.gr>
2022-06-21 20:56:16 +0300

    Remove HomeScreen from AuthNavigator

--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import HomeScreen from '../screens/HomeScreen'
 import LoginScreen from '../screens/LoginScreen'
 import RegisterScreen from '../screens/RegisterScreen'
+import AppNavigator from './AppNavigator'
 
 const Stack = createStackNavigator();
 
 const AuthNavigator = (props) => {
     return (
         <Stack.Navigator
+            initialRouteName='LoginScreen'
             screenOptions={{
                 headerShown: false,
                 headerTransparent: true
             }}
         >
-            <Stack.Screen name='HomeScreen' component={HomeScreen} />
             <Stack.Screen name='LoginScreen' component={LoginScreen} />
             <Stack.Screen name='RegisterScreen' component={RegisterScreen} />
         </Stack.Navigator>
