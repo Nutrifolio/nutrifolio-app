11f40e439a59b0a696aec23c33b66962ca42fdd1
sotirangelo <t8190001@aueb.gr>
2022-06-21 17:53:12 +0300

    Add white to colors

--- styles/colors.js
+++ styles/colors.js
@@ -2,7 +2,8 @@ const colors = {
     primary: '#5BAB34',
     secondary: '#DFE9D0',
     black: '#212529',
-    grey: '#777777'
+    grey: '#777777',
+    white: '#FFFFFF'
 }
 
 export default colors;
