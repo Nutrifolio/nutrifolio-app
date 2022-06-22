f93df338d5d02ddb1cbdd6f5e21567420d6980a3
sotirangelo <t8190001@aueb.gr>
2022-06-22 16:41:37 +0300

    Add onPress prop to NutriLink

--- components/NutriLink.js
+++ components/NutriLink.js
@@ -5,13 +5,17 @@ import colors from '../styles/colors';
 import NutriText from './NutriText';
 
 const NutriLink = (props) => (
-    <TouchableOpacity style={{ ...styles.container, ...props.style }}>
+    <TouchableOpacity
+        onPress={props.onPress}
+        style={{ ...styles.container, ...props.style }}
+    >
         <NutriText style={styles.text}>{props.text}</NutriText>
     </TouchableOpacity>
 );
 
 NutriLink.propTypes = {
-    text: propTypes.string,
+    text: propTypes.string.isRequired,
+    onPress: propTypes.func.isRequired,
     style: propTypes.object,
 };
 
