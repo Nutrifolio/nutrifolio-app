8bd35272215f15f290542e9632506c4fc26d18e3
sotirangelo <t8190001@aueb.gr>
2022-06-23 18:42:23 +0300

    Add initial Log In functionality in LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import NutriText from '../components/NutriText';
-import Screen from '../components/Screen';
-import colors from '../styles/colors';
-import { NutriForm, NutriFormField, SubmitButton } from '../components/forms';
 import * as Yup from 'yup';
-import routes from '../navigation/routes';
+import AuthContext from '../auth/context';
+import colors from '../styles/colors';
+import ErrorMessage from '../components/forms/ErrorMessage';
+import NutriText from '../components/NutriText';
 import propTypes from 'prop-types';
+import routes from '../navigation/routes';
+import Screen from '../components/Screen';
 import TextButton from '../components/buttons/TextButton';
+import React, { useState, useContext } from 'react';
+import { login } from '../api/accountApi';
+import { StyleSheet, View } from 'react-native';
+import { NutriForm, NutriFormField, SubmitButton } from '../components/forms';
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label('Email'),
@@ -15,6 +18,24 @@ const validationSchema = Yup.object().shape({
 });
 
 const LoginScreen = (props) => {
+    const authContext = useContext(AuthContext);
+    const [loginError, setLoginError] = useState(false);
+
+    const handleSubmit = async (values) => {
+        const body = { username: values.email, password: values.password };
+        const response = await login(body);
+        const data = await response.json();
+
+        if (!response.ok) {
+            setLoginError(true);
+        } else {
+            setLoginError(false);
+            const accessToken = data['access_token'];
+            authContext.setAccessToken(accessToken);
+            console.log(accessToken);
+        }
+    };
+
     const { navigation } = props;
     const welcomeMessage = 'Welcome Back';
     return (
@@ -23,9 +44,13 @@ const LoginScreen = (props) => {
                 <NutriText style={styles.title}>{welcomeMessage}</NutriText>
                 <NutriForm
                     initialValues={{ email: '', password: '' }}
-                    onSubmit={(values) => console.log(values)}
+                    onSubmit={handleSubmit}
                     validationSchema={validationSchema}
                 >
+                    <ErrorMessage
+                        error={'Error logging in'}
+                        visible={loginError}
+                    />
                     <NutriFormField
                         placeholder='Email'
                         name='email'
@@ -48,7 +73,7 @@ const LoginScreen = (props) => {
                 </NutriForm>
                 <TextButton
                     text='Not a member yet?'
-                    style={styles.link}
+                    textStyle={styles.link}
                     onPress={() => navigation.navigate(routes.REGISTER)}
                 />
             </View>
@@ -75,7 +100,7 @@ const styles = StyleSheet.create({
         marginBottom: 30,
     },
     link: {
-        marginTop: 20,
+        marginTop: 50,
         color: colors.primary,
         textDecorationLine: 'underline',
     },
