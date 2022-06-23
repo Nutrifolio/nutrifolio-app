33d6c3be1e0ca47f93df2954955978c11835e48f
sotirangelo <t8190001@aueb.gr>
2022-06-23 22:30:39 +0300

    Fix useApi hook

--- hooks/useApi.js
+++ hooks/useApi.js
@@ -7,10 +7,19 @@ const useApi = (apiFunc) => {
 
     const request = async (...args) => {
         setLoading(true);
-        apiFunc(...args)
-            .then((data) => setData(data))
-            .catch((err) => setError(err));
+        const response = await apiFunc(...args);
+        const data = await response.json();
         setLoading(false);
+        if (!response.ok) {
+            if (data) {
+                setError(data.detail);
+            } else {
+                setError('An unexpected error occured. Try again later.');
+            }
+        } else {
+            setData(data);
+        }
+        return response;
     };
 
     return { data, error, loading, request };
