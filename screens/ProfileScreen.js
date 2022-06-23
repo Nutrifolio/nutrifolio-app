988c39d3fa06259c80e5bedc7f720193abc0fdc4
sotirangelo <t8190001@aueb.gr>
2022-06-23 20:33:10 +0300

    Add temporary log out button in ProfileScreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import useAuth from '../hooks/useAuth';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 import Screen from '../components/Screen';
 
 const ProfileScreen = () => {
+    const { logOut } = useAuth();
+
+    const handleLogOut = () => {
+        logOut();
+    };
+
     return (
         <Screen>
             <View style={styles.container}>
                 <Text>ProfileScreen</Text>
+                <Button title='Log out' onPress={handleLogOut} />
             </View>
         </Screen>
     );
