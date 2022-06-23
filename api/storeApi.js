d16b31f461ee8fa19ffe1a526cf0e93f581c0a24
sotirangelo <t8190001@aueb.gr>
2022-06-24 02:39:42 +0300

    Increase storeApi default distance

--- api/storeApi.js
+++ api/storeApi.js
@@ -2,7 +2,7 @@ import endpoints from './endpoints';
 
 const getStore = async (storeId) => fetch(`${endpoints.STORES}/${storeId}`);
 
-const searchStore = async (q, lat, lng, distance = 5) =>
+const searchStore = async (q, lat, lng, distance = 200) =>
     fetch(
         `${endpoints.SEARCH}?q=${q}&lat=${lat}&lng=${lng}&distance=${distance}`,
     );
