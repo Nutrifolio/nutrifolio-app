e293b0349904903e5dd5ff4043b1695558b9eeb4
sotirangelo <t8190001@aueb.gr>
2022-06-23 22:16:10 +0300

    Add register functionality in RegisterScreen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,16 @@
-import propTypes from 'prop-types';
-import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
-import NutriText from '../components/NutriText';
-import { NutriForm, NutriFormField, SubmitButton } from '../components/forms';
 import * as Yup from 'yup';
 import colors from '../styles/colors';
+import NutriText from '../components/NutriText';
+import propTypes from 'prop-types';
 import routes from '../navigation/routes';
-
 import Screen from '../components/Screen';
 import TextButton from '../components/buttons/TextButton';
+import useAuth from '../hooks/useAuth';
+import React, { useState } from 'react';
+import { signup } from '../api/accountApi';
+import { StyleSheet, ScrollView } from 'react-native';
+import { NutriForm, NutriFormField, SubmitButton } from '../components/forms';
+import ErrorMessage from '../components/forms/ErrorMessage';
 
 const validationSchema = Yup.object().shape({
     firstName: Yup.string().required().label('First Name'),
@@ -21,8 +23,46 @@ const validationSchema = Yup.object().shape({
 });
 
 const RegisterScreen = (props) => {
-    const { navigation } = props;
+    const { logIn } = useAuth();
+    const [error, setError] = useState();
     const registerMessage = 'Sign up';
+
+    const handleSubmit = async (userInfo) => {
+        const { firstName, lastName, email, password, confirmPassword } =
+            userInfo;
+
+        const body = {
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            password: password,
+            conf_password: confirmPassword,
+        };
+
+        const response = await signup(body);
+        const data = await response.json();
+
+        // Error handling
+        if (!response.ok) {
+            if (data) {
+                setError(data.detail[0]);
+            } else {
+                setError('An unexpected error occured. Try again later.');
+            }
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                    setError(null);
+                    return resolve;
+                }, 6000),
+            );
+            return;
+        }
+
+        // Log in
+        setError(null);
+        logIn(data.access_token);
+    };
+
     return (
         <Screen>
             <ScrollView
@@ -38,9 +78,10 @@ const RegisterScreen = (props) => {
                         password: '',
                         confirmPassword: '',
                     }}
-                    onSubmit={(values) => console.log(values)}
+                    onSubmit={handleSubmit}
                     validationSchema={validationSchema}
                 >
+                    <ErrorMessage error={error} />
                     <NutriFormField
                         placeholder='First Name'
                         name='firstName'
@@ -87,7 +128,7 @@ const RegisterScreen = (props) => {
                 <TextButton
                     text='Already have an account?'
                     textStyle={styles.link}
-                    onPress={() => navigation.navigate(routes.LOGIN)}
+                    onPress={() => props.navigation.navigate(routes.LOGIN)}
                 />
             </ScrollView>
         </Screen>
