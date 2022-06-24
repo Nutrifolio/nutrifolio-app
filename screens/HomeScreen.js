08110bc5b562593528bbd993669f956859d3863a
sotirangelo <t8190001@aueb.gr>
2022-06-24 06:05:47 +0300

    Add navigation from home to product details

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,15 @@
 import colors from '../styles/colors';
 import NutriText from '../components/NutriText';
-import { getFavorites, getRecents } from '../api/productApi';
 import ProductCard from '../components/ProductCard';
 import propTypes from 'prop-types';
+import routes from '../navigation/routes';
 import Screen from '../components/Screen';
 import SmallButton from '../components/buttons/SmallButton';
 import useApi from '../hooks/useApi';
 import useAuth from '../hooks/useAuth';
 import UserContext from '../auth/userContext';
 import React, { useEffect, useContext } from 'react';
+import { getFavorites, getRecents } from '../api/productApi';
 import { StyleSheet, View, ScrollView } from 'react-native';
 
 const welcomeMessage = 'Welcome back,';
@@ -17,7 +18,7 @@ const titleList1 = 'FAVORITES';
 const titleList2 = 'RECENTS';
 const smallButtonText = 'View All';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
     const { products, setProducts } = useContext(UserContext);
     const { accessToken } = useAuth();
     const recentsApi = useApi(getRecents);
@@ -57,21 +58,24 @@ const HomeScreen = () => {
 
                 {products &&
                     'favorites' in products &&
-                    products.favorites
-                        .slice(0, 2)
-                        .map((item) => (
-                            <ProductCard
-                                key={item.id}
-                                title={item.title}
-                                calories={item.calories}
-                                description={item.description}
-                                distance={item.distance}
-                                price={item.price}
-                                productImage={item.productImage}
-                                store={item.store}
-                                storeImage={item.storeImage}
-                            />
-                        ))}
+                    products.favorites.slice(0, 2).map((item) => (
+                        <ProductCard
+                            key={item.id}
+                            title={item.title}
+                            calories={item.calories}
+                            description={item.description}
+                            distance={item.distance}
+                            price={item.price}
+                            productImage={item.productImage}
+                            store={item.store}
+                            storeImage={item.storeImage}
+                            onPress={() =>
+                                navigation.navigate(routes.PRODUCT, {
+                                    id: item.id,
+                                })
+                            }
+                        />
+                    ))}
 
                 <View style={styles.listHeader}>
                     <NutriText style={styles.listTitle}>{titleList2}</NutriText>
@@ -83,21 +87,24 @@ const HomeScreen = () => {
 
                 {products &&
                     'recents' in products &&
-                    products.recents
-                        .slice(0, 2)
-                        .map((item) => (
-                            <ProductCard
-                                key={item.id}
-                                title={item.title}
-                                calories={item.calories}
-                                description={item.description}
-                                distance={item.distance}
-                                price={item.price}
-                                productImage={item.productImage}
-                                store={item.store}
-                                storeImage={item.storeImage}
-                            />
-                        ))}
+                    products.recents.slice(0, 2).map((item) => (
+                        <ProductCard
+                            key={item.id}
+                            title={item.title}
+                            calories={item.calories}
+                            description={item.description}
+                            distance={item.distance}
+                            price={item.price}
+                            productImage={item.productImage}
+                            store={item.store}
+                            storeImage={item.storeImage}
+                            onPress={() =>
+                                navigation.navigate(routes.PRODUCT, {
+                                    id: item.id,
+                                })
+                            }
+                        />
+                    ))}
             </ScrollView>
         </Screen>
     );
