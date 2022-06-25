38501623dc11a638d3fc4a858d167133e8ab7607
sotirangelo <t8190001@aueb.gr>
2022-06-26 01:57:03 +0300

    Add useApi hook to RegisterScreen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,24 +1,25 @@
 import * as Yup from 'yup';
+import ActivityIndicator from '../components/ActivityIndicator';
 import colors from '../styles/colors';
+import ErrorMessage from '../components/forms/ErrorMessage';
 import NutriText from '../components/NutriText';
 import propTypes from 'prop-types';
 import routes from '../navigation/routes';
 import Screen from '../components/Screen';
 import TextButton from '../components/buttons/TextButton';
+import useApi from '../hooks/useApi';
 import useAuth from '../hooks/useAuth';
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { signup } from '../api/accountApi';
 import { StyleSheet, ScrollView } from 'react-native';
 import { NutriForm, NutriFormField, SubmitButton } from '../components/forms';
-import ErrorMessage from '../components/forms/ErrorMessage';
-import ActivityIndicator from '../components/ActivityIndicator';
 
 const validationSchema = Yup.object().shape({
-    firstName: Yup.string().required().label('First Name'),
-    lastName: Yup.string().required().label('Last Name'),
+    first_name: Yup.string().required().label('First Name'),
+    last_name: Yup.string().required().label('Last Name'),
     email: Yup.string().required().email().label('Email'),
-    password: Yup.string().required().min(4).label('Password'),
-    confirmPassword: Yup.string()
+    password: Yup.string().required().min(12).label('Password'),
+    conf_password: Yup.string()
         .oneOf([Yup.ref('password')], 'Passwords must match')
         .required('Passwords must match'),
 });
@@ -26,41 +27,16 @@ const validationSchema = Yup.object().shape({
 const registerMessage = 'Sign up';
 
 const RegisterScreen = (props) => {
-    const [error, setError] = useState();
-    const [loading, setLoading] = useState(false);
+    const { data, loading, error, request } = useApi(signup);
     const { logIn } = useAuth();
 
     const handleSubmit = async (userInfo) => {
-        const { firstName, lastName, email, password, confirmPassword } =
-            userInfo;
-
-        const body = {
-            first_name: firstName,
-            last_name: lastName,
-            email: email,
-            password: password,
-            conf_password: confirmPassword,
-        };
-        setLoading(true);
-        const response = await signup(body);
-        const data = await response.json();
-
-        // Error handling
-        if (!response.ok) {
-            if (data) {
-                setError(data.detail[0]);
-            } else {
-                setError('An unexpected error occured. Try again later.');
-            }
-            setLoading(false);
-            return;
-        }
-
-        // Log in
-        setError(null);
-        setLoading(false);
-        logIn(data.access_token);
+        await request(userInfo);
     };
+
+    useEffect(() => {
+        if (data) logIn(data.access_token);
+    }, [data]);
 
     return (
         <>
@@ -75,11 +51,11 @@ const RegisterScreen = (props) => {
                     </NutriText>
                     <NutriForm
                         initialValues={{
-                            firstName: '',
-                            lastName: '',
+                            first_name: '',
+                            last_name: '',
                             email: '',
                             password: '',
-                            confirmPassword: '',
+                            conf_password: '',
                         }}
                         onSubmit={handleSubmit}
                         validationSchema={validationSchema}
@@ -87,14 +63,14 @@ const RegisterScreen = (props) => {
                         <ErrorMessage error={error} />
                         <NutriFormField
                             placeholder='First Name'
-                            name='firstName'
+                            name='first_name'
                             textContentType='givenName'
                             iconName='account-circle'
                         />
 
                         <NutriFormField
                             placeholder='Last Name'
-                            name='lastName'
+                            name='last_name'
                             textContentType='familyName'
                             iconName='account-circle'
                         />
@@ -119,7 +95,7 @@ const RegisterScreen = (props) => {
 
                         <NutriFormField
                             placeholder='Confirm Password'
-                            name='confirmPassword'
+                            name='conf_password'
                             autoCapitalize='none'
                             iconName='lock'
                             textContentType='password'
